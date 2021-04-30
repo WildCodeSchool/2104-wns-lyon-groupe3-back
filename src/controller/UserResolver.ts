9f42bd7ad0ff2945bbb0dbc53ef35ea742c862bb
Theraen <quentin.barraud2@gmail.com>
2021-04-30 09:30:05 +0200

    Update all method in UserResolver for add return type function

--- src/controller/UserResolver.ts
+++ src/controller/UserResolver.ts
@@ -1,34 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Arg, Query, Resolver } from "type-graphql";
 import { User } from "../model/graphql/User";
 import UserModel from "../model/UserModel";
-import mongoose from 'mongoose';
-import { isAbstractType } from "graphql";
 
 @Resolver(User)
 export class UserResolver {
     @Query(returns => User, {nullable: true})
-    public async getUserById(@Arg('id', type => String) id: String) {
+    public async getUserById(@Arg('id', type => String) id: string):Promise<void> {
         return await UserModel.findById(id);
     }
 
     @Query(returns => User, {nullable: true})
-    public async getUserByEmail(@Arg('email', type => String) email: string) {
+    public async getUserByEmail(@Arg('email', type => String) email: string):Promise<void> {
         return await UserModel.findOne({ email: email })||null;
     }
 
     @Query(returns => [User])
-    public async getUsersByRole(@Arg('role', type => String) role: string) {
+    public async getUsersByRole(@Arg('role', type => String) role: string):Promise<Array<void>> {
         return await UserModel.find({ role: role });
     }
 
     @Query(returns => [User])
-    public async getUsersByIsActive(@Arg('isActive', type => String) isActive: string) {
+    public async getUsersByIsActive(@Arg('isActive', type => String) isActive: string):Promise<Array<void>> {
         console.log(isActive);
         return await UserModel.find({ isActive: isActive });
     }
 
     @Query(returns => [User])
-    public async getAllUsers() {
+    public async getAllUsers():Promise<Array<void>> {
         return await UserModel.find() 
     }
 }
